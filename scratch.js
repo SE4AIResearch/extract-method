7cf048b68db9a1841118b0bf246d980fb7ccc39c
SWEsnowman <100440384+SWEsnowman@users.noreply.github.com>
2023-07-19 14:00:18 -0400

    inserted publications and added watch/forks from github

--- scratch.js
+++ scratch.js
@@ -1,8 +1,12 @@
 async function requestGitHub() {
   const response = await fetch("https://api.github.com/repos/JetBrains-Research/anti-copy-paster");
   const data = await response.json();
-
-  console.log(data.watchers_count);
+  const forkElement = document.getElementById("Forks");
+  const html = `<p>${data.forks_count}</p>`;
+  forkElement.insertAdjacentHTML("afterend", html);
+  const watchElement = document.getElementById("Watched")
+  const html1 = `<p>${data.watchers_count}`;
+  watchElement.insertAdjacentHTML("afterend", html1);
   return data;
 }
 requestGitHub();
