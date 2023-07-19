e799a01788091ff9016ea046cfc2141f9729aa08
SWEsnowman <100440384+SWEsnowman@users.noreply.github.com>
2023-07-19 14:26:02 -0400

    added sourceforge downloads

--- scratch.js
+++ scratch.js
@@ -5,9 +5,24 @@ async function requestGitHub() {
   const html = `<p>${data.forks_count}</p>`;
   forkElement.insertAdjacentHTML("afterend", html);
   const watchElement = document.getElementById("Watched")
-  const html1 = `<p>${data.watchers_count}`;
+  const html1 = `<p>${data.watchers_count}</p>`;
   watchElement.insertAdjacentHTML("afterend", html1);
-  return data;
 }
+
+async function requestSourceForge() {;
+let currentDate = new Date();
+const day = currentDate.getDate();
+const month = currentDate.getMonth() + 1;
+const year = currentDate.getFullYear();
+const today = `${year}-${month}-${day}`;
+const url = "https://sourceforge.net/projects/anti-copy-paster/files/stats/json?start_date=2023-06-30&end_date=" + today;
+const response = await fetch(url);
+const data = await response.json();
+const downloadElement = document.getElementById("Download");
+const html = `<p>${data.total}</p>`;
+downloadElement.insertAdjacentHTML("afterend", html);
+}
+
+requestSourceForge();
 requestGitHub();
 
