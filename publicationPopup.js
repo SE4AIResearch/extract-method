d2ddb7af08b25fe66b9b396bb683244e5c420aea
SWEsnowman <100440384+SWEsnowman@users.noreply.github.com>
2023-07-25 18:25:55 -0400

    continue implementing bootstrap

--- publicationPopup.js
+++ publicationPopup.js
@@ -1,17 +1,3 @@
-function openCitation(Id){
-const popup = document.getElementById(Id);
-popup.style.visibility = 'visible';
-const page =  document.getElementById("blur");
-page.style.filter = 'blur(2px)';
-}
-
-function closeCitation(Id){
-const popup = document.getElementById(Id);
-popup.style.visibility = 'hidden';
-const page =  document.getElementById("blur");
-page.style.filter = 'blur(0px)';
-}
-
 function clipboardCopy(text){
 navigator.clipboard.writeText(text);
 }
